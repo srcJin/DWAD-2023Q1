731f211d3f5333b2f21d557e005d9dba66a291b7
Bee Lee Ong <beelee93@gmail.com>
2023-02-20 16:47:08 +0000

    Register new views into router

--- routes/landing.js
+++ routes/landing.js
@@ -5,4 +5,12 @@ router.get('/', (_, res) => {
   res.render('landing/index');
 });
 
+router.get('/about-us', (_, res) => {
+  res.render('landing/about-us');
+});
+
+router.get('/contact-us', (_, res) => {
+  res.render('landing/contact-us');
+});
+
 module.exports = router;
