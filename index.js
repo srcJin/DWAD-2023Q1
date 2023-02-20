4ce60f59636d34ed18ff49d0c590dfd0a5de6c1a
Bee Lee Ong <beelee93@gmail.com>
2023-02-20 16:24:26 +0000

    Define boilerplate code

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const hbs = require('hbs'); // https://www.npmjs.com/package/hbs
+const wax = require('wax-on'); // https://www.npmjs.com/package/wax-on
+
+// create an instance of express app
+const app = express();
+
+// set the view engine
+app.set('view engine', 'hbs'); // Add support for handlebars templating engine {{ ... }}
+
+// static folder
+app.use(express.static('public'));
+
+// setup wax-on
+wax.on(hbs.handlebars); // register wax-on helpers with handlerbars
+wax.setLayoutPath('./views/layout');
+
+// enable forms
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+// register routes
+app.get('/', (_, res) => {
+  res.send("It's alive!");
+});
+
+async function main() {}
+main();
+
+app.listen(3000, () => {
+  console.log('Server has started');
+});
