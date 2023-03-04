6f0524cb580de954404b8225954b248062e27b8e
kunxin-chor <chorkunxin@yahoo.com>
2023-03-04 03:37:50 +0000

    Added forms

--- routes/landing.js
+++ routes/landing.js
@@ -1,6 +1,8 @@
 const express = require('express');
+// use `express.Router()` function to create a new router object
 const router = express.Router();
 
+// instead of `app.get` or `app.post` we use `router.get`, `router.post` etc.
 router.get('/', (_, res) => {
   res.render('landing/index');
 });
@@ -13,4 +15,6 @@ router.get('/contact-us', (_, res) => {
   res.render('landing/contact-us');
 });
 
+// make sure to export our created router object
+// whatever we export, other JS files in nodejs can use
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { createProductForm, bootstrapField } = require("../forms")
 const router = express.Router();
 
 const {
@@ -11,6 +12,43 @@ router.get('/', async (req, res) => {
   res.render('products/index', {
     products: products.toJSON(),
   });
+})
+
+// display the form for creating a new product
+router.get("/create", function(req,res){
+  const productForm = createProductForm();
+  res.render("products/create", {
+    "form": productForm.toHTML(bootstrapField)
+  })
+})
+
+// process the submitted form
+router.post("/create", async function(req,res){
+  // recreate the form object first
+  const productForm = createProductForm();
+
+  // handle will process the form for us
+  // first parameter: req
+  // second parameter: object with three functions
+  productForm.handle(req, {
+    "success": function(form) {
+       // will be called if the form has no user errors
+       console.log(form.data);
+       res.send("form recieved");
+    },
+    "empty": function(form) {
+      // will be called if the form is submitted with no input
+      res.render("products/create",{
+        "form": form.toHTML(bootstrapField)
+      })
+    },
+    "error": function(form) {
+      // the form has user errors (i.e validation errors)
+      res.render("products/create",{
+        "form": form.toHTML(bootstrapField)
+      })
+    }
+  })
 })
 
 module.exports = router;
