97413b0d38fc6e4bafdbe12fd19f3e536246fda5
Bee Lee Ong <beelee93@gmail.com>
2023-04-07 16:12:07 +0000

    add dal for products and cleanup

---
+++ dal/products.js
@@ -0,0 +1,95 @@
+const { Products, Categories, Tags } = require("../models");
+
+exports.getAllCategories = () =>
+  Categories.fetchAll().map((category) => [
+    category.get("id"),
+    category.get("name"),
+  ]);
+
+exports.getAllTags = () =>
+  Tags.fetchAll().map((tag) => [tag.get("id"), tag.get("name")]);
+
+exports.getProductByID = (productId) =>
+  Products.where({ id: parseInt(productId) }).fetch({
+    require: true,
+    withRelated: ["tags", "category"],
+  });
+
+exports.searchProduct = ({ name, categoryId, minCost, maxCost, tags }) => {
+  const query = Products.collection();
+
+  if (name) {
+    query.where("name", "like", `%${name}%`);
+  }
+  if (categoryId && categoryId !== "0") {
+    query.where("category_id", "=", categoryId);
+  }
+  if (minCost) {
+    query.where("cost", ">=", minCost);
+  }
+  if (maxCost) {
+    query.where("cost", ">=", maxCost);
+  }
+  if (tags) {
+    // knex.query('join', '<JOIN_TABLE_NAME>', '<ORIGINAL_TABLE_COLUMN>', '<JOIN_TABLE_COLUMN>')
+    // This creates an "inner join `JOIN_TABLE_NAME` on `ORIGINAL_TABLE_COLUMN` = `<JOIN_TABLE_COLUMN>`" statement
+    // `join` is a function exposed by the Knex Query Builder https://knexjs.org/guide/query-builder.html#join
+    query
+      .query("join", "products_tags", "products.id", "product_id")
+      .where("tag_id", "in", tags.split(","));
+  }
+
+  return query.fetch({ withRelated: ["category", "tags"] });
+};
+
+exports.createProduct = async (
+  name,
+  cost,
+  description,
+  categoryId,
+  tags,
+  imageUrl
+) => {
+  const product = new Products({
+    name,
+    cost,
+    description,
+    category_id: categoryId,
+    image_url: imageUrl,
+  });
+
+  // remember to save the newly created product
+  await product.save();
+
+  if (tags) {
+    await product.tags().attach(tags.split(","));
+  }
+
+  return product;
+};
+
+exports.updateProductByID = async (productId, updateParams) => {
+  const product = await this.getProductByID(productId);
+  const { tags, ...productData } = updateParams;
+
+  product.set(productData); // save everything in form.data to the product
+  // IMPORTANT: all the keys in form.data must exist
+  // as columns in the products table for this shortcut to work
+  await product.save();
+
+  // update the tags by first removing the unnecessary ones
+  const tagIds = tags.split(",");
+  const existingTagIds = product.related("tags").pluck("id");
+  const toRemove = existingTagIds.filter((tag) => !tagIds.includes(tag));
+
+  await product.tags().detach(toRemove);
+  await product.tags().attach(tagIds);
+
+  return product;
+};
+
+exports.deleteProductByID = async (productId) => {
+  // SELECT * where products where id = '1'
+  const product = await this.getProductByID(productId);
+  await product.destroy();
+};
--- routes/products.js
+++ routes/products.js
@@ -1,100 +1,82 @@
-const express = require('express');
-const { createProductForm, bootstrapField, createSearchForm } = require("../forms")
-const { checkIfAuthenticated } = require('../middlewares');
+const express = require("express");
+const {
+  createProductForm,
+  bootstrapField,
+  createSearchForm,
+} = require("../forms");
+const {
+  searchProduct,
+  getAllCategories,
+  getAllTags,
+  createProduct,
+  getProductByID,
+  updateProductByID,
+  deleteProductByID,
+} = require("../dal/products");
+const { checkIfAuthenticated } = require("../middlewares");
 
 const router = express.Router();
 
-const {
-  Products, Categories, Tags
-} = require('../models');
-
-router.get('/', checkIfAuthenticated, async (req, res) => {
+router.get("/", checkIfAuthenticated, async (req, res) => {
   // Get all categories and tags
-  let categoryObjects = await Categories.fetchAll();
-  let categories = categoryObjects.map((category) => [category.get('id'), category.get('name')]);
-  categories.unshift(['0', '----']); // Add empty category to represent no category filter
+  let categories = await getAllCategories();
+  categories.unshift(["0", "----"]); // Add empty category to represent no category filter
 
-  let tagObjects = await Tags.fetchAll();
-  let tags = tagObjects.map((tag) => [tag.get('id'), tag.get('name')]);
+  let tags = await getAllTags();
 
   let searchForm = createSearchForm(categories, tags);
-  let query = Products.collection();
   searchForm.handle(req, {
     empty: async (form) => {
       // Empty form submission - we take it as no filter so we fetch everything
-      let products = await query.fetch({withRelated: ['category', 'tags']});
-      res.render('products/index', {
-        form: searchForm.toHTML(bootstrapField),
+      let products = await searchProduct({});
+      res.render("products/index", {
+        form: form.toHTML(bootstrapField),
         products: products.toJSON(),
       });
     },
     error: async (form) => {
       // When there is error, we take it as no filter so we fetch everything
-      let products = await query.fetch({withRelated: ['category', 'tags']});
-      res.render('products/index', {
-        form: searchForm.toHTML(bootstrapField),
+      let products = await searchProduct({});
+      res.render("products/index", {
+        form: form.toHTML(bootstrapField),
         products: products.toJSON(),
       });
     },
     success: async (form) => {
-      if (form.data.name) {
-        query.where('name', 'like', `%${form.data.name}%`);
-      }
-
-      if (form.data.category_id && form.data.category_id !== '0') {
-        query.where('category_id', '=', form.data.category_id);
-      }
-
-      if (form.data.min_cost) {
-        query.where('cost', '>=', form.data.min_cost);
-      }
-
-      if (form.data.max_cost) {
-        query.where('cost', '>=', form.data.min_cost);
-      }
-
-      if (form.data.tags) {
-        // knex.query('join', '<JOIN_TABLE_NAME>', '<ORIGINAL_TABLE_COLUMN>', '<JOIN_TABLE_COLUMN>')
-        // This creates an "inner join `JOIN_TABLE_NAME` on `ORIGINAL_TABLE_COLUMN` = `<JOIN_TABLE_COLUMN>`" statement
-        // `join` is a function exposed by the Knex Query Builder https://knexjs.org/guide/query-builder.html#join
-        query.query('join', 'products_tags', 'products.id', 'product_id')
-          .where('tag_id', 'in', form.data.tags.split(','));
-      }
-
-      let products = await query.fetch({withRelated: ['category', 'tags']});
-      res.render('products/index', {
+      let products = await searchProduct({
+        name: form.data.name,
+        categoryId: form.data.category_id,
+        minCost: form.data.min_cost,
+        maxCost: form.data.max_cost,
+        tags: form.data.tags,
+      });
+      res.render("products/index", {
         form: searchForm.toHTML(bootstrapField),
         products: products.toJSON(),
       });
-    }
-  })
-})
+    },
+  });
+});
 
 // display the form for creating a new product
-router.get("/create", checkIfAuthenticated, async function(req,res){
-  let categoryObjects = await Categories.fetchAll();
-  let categories = categoryObjects.map((category) => [category.get('id'), category.get('name')]);
-
-  let tagObjects = await Tags.fetchAll();
-  let tags = tagObjects.map((tag) => [tag.get('id'), tag.get('name')]);
+router.get("/create", checkIfAuthenticated, async function (req, res) {
+  let categories = await getAllCategories();
+  let tags = await getAllTags();
 
   const productForm = createProductForm(categories, tags);
   res.render("products/create", {
-    "form": productForm.toHTML(bootstrapField),
+    form: productForm.toHTML(bootstrapField),
     cloudinaryName: process.env.CLOUDINARY_NAME,
     cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
-    cloudinaryPreset: process.env.CLOUDINARY_UPLOAD_PRESET
-  })
-})
+    cloudinaryPreset: process.env.CLOUDINARY_UPLOAD_PRESET,
+  });
+});
 
 // process the submitted form
-router.post("/create", checkIfAuthenticated, async function(req,res){
+router.post("/create", checkIfAuthenticated, async function (req, res) {
   // recreate the form object first
-  let categoryObjects = await Categories.fetchAll();
-  let categories = categoryObjects.map((category) => [category.get('id'), category.get('name')]);
-
-  let tagObjects = await Tags.fetchAll();
-  let tags = tagObjects.map((tag) => [tag.get('id'), tag.get('name')]);
+  let categories = await getAllCategories();
+  let tags = await getAllTags();
 
   const productForm = createProductForm(categories, tags);
 
@@ -102,160 +84,127 @@ router.post("/create", checkIfAuthenticated, async function(req,res){
   // first parameter: req
   // second parameter: object with three functions
   productForm.handle(req, {
-    "success": async function(form) {
-       // will be called if the form has no user errors
+    success: async function (form) {
+      // will be called if the form has no user errors
 
-       // one instance of Products will represent one row
-       // in the Products table
-       const {tags, ...productData} = form.data;
-       const product = new Products(productData);
+      // one instance of Products will represent one row
+      // in the Products table
+      let product = await createProduct(
+        form.data.name,
+        form.data.cost,
+        form.data.description,
+        form.data.category_id,
+        form.data.tags,
+        form.data.image_url
+      );
 
-       // remember to save the newly created product
-       await product.save();
-
-       if (tags) {
-        await product.tags().attach(tags.split(','));
-       }
-
-       console.log(form.data);
-       req.flash('success_messages', `New Product ${product.get('name')} has been created.`);
-       res.redirect("/products");
-    },
-    "empty": function(form) {
-      // will be called if the form is submitted with no input
-      res.render("products/create",{
-        "form": form.toHTML(bootstrapField)
-      })
-    },
-    "error": function(form) {
-      // the form has user errors (i.e validation errors)
-      res.render("products/create",{
-        "form": form.toHTML(bootstrapField)
-      })
-    }
-  })
-})
-
-
-// display the form for modifying a product
-router.get("/update/:product_id", checkIfAuthenticated, async function(req,res){
-  const productId = req.params.product_id;
-  // SELECT * where products where id = '1'
-  const product = await Products.where({
-    "id": productId
-  }).fetch({
-    "require": true,
-    "withRelated": ['tags'],
-  });
-
-  // create product form
-  let categoryObjects = await Categories.fetchAll();
-  let categories = categoryObjects.map((category) => [category.get('id'), category.get('name')]);
-
-  let tagObjects = await Tags.fetchAll();
-  let tags = tagObjects.map((tag) => [tag.get('id'), tag.get('name')]);
-
-  const productForm = createProductForm(categories, tags);
-
-  // set the initial value for each field
-  productForm.fields.name.value = product.get('name');
-  productForm.fields.cost.value = product.get('cost');
-  productForm.fields.description.value = product.get('description');
-  productForm.fields.category_id.value = product.get('category_id');
-  productForm.fields.image_url.value = product.get('image_url');
-
-  // https://bookshelfjs.org/api.html#Collection-instance-pluck - returns an array of values based on the selected column name
-  const selectedTags = await product.related('tags').pluck('id');
-  productForm.fields.tags.value = selectedTags;
-
-  res.render("products/update", {
-    "form": productForm.toHTML(bootstrapField),
-    "product": product.toJSON(),  //  convert the Product bookshelf object to the JSON format so hbs files can use it
-    "cloudinaryName": process.env.CLOUDINARY_NAME,
-    "cloudinaryApiKey": process.env.CLOUDINARY_API_KEY,
-    "cloudinaryPreset": process.env.CLOUDINARY_UPLOAD_PRESET,
-  })
-
-})
-
-// update the product
-router.post("/update/:product_id", checkIfAuthenticated, async function(req,res){
-  let categoryObjects = await Categories.fetchAll();
-  let categories = categoryObjects.map((category) => [category.get('id'), category.get('name')]);
-
-  let tagObjects = await Tags.fetchAll();
-  let tags = tagObjects.map((tag) => [tag.get('id'), tag.get('name')]);
-
-  const productForm = createProductForm(categories, tags);
-
-  // fetch the product that we want to edit
-  const productId = req.params.product_id;
-  const product = await Products.where({
-    "id": productId
-  }).fetch({
-    "require": true,
-    "withRelated": ['tags'],
-  });
-  productForm.handle(req, {
-    "success": async function(form) {
-      const {tags, ...productData} = form.data;
-
-      product.set(productData);  // save everything in form.data to the product
-                               // IMPORTANT: all the keys in form.data must exist
-                               // as columns in the products table for this shortcut to work
-      await product.save();
-
-      // update the tags by first removing the unnecessary ones
-      const tagIds = tags.split(',');
-      const existingTagIds = product.related('tags').pluck('id');
-      const toRemove = existingTagIds.filter(tag => !tagIds.includes(tag));
-
-      await product.tags().detach(toRemove);
-      await product.tags().attach(tagIds);
-
+      req.flash(
+        "success_messages",
+        `New Product ${product.get("name")} has been created.`
+      );
       res.redirect("/products");
     },
-    "error": function(form) {
-      res.render("/products/update", {
-        "form": form,
-        "prodct": product.toJSON()
-      })
+    empty: function (form) {
+      // will be called if the form is submitted with no input
+      res.render("products/create", {
+        form: form.toHTML(bootstrapField),
+      });
     },
-    "empty": function(form) {
-      res.render("/products/update", {
-        "form": form,
-        "prodct": product.toJSON()
-      })
-    }
-  })
-})
+    error: function (form) {
+      // the form has user errors (i.e validation errors)
+      res.render("products/create", {
+        form: form.toHTML(bootstrapField),
+      });
+    },
+  });
+});
+
+// display the form for modifying a product
+router.get(
+  "/update/:product_id",
+  checkIfAuthenticated,
+  async function (req, res) {
+    const productId = req.params.product_id;
+    let product = await getProductByID(productId);
+
+    // create product form
+    let categories = await getAllCategories();
+    let tags = await getAllTags();
+    const productForm = createProductForm(categories, tags);
+
+    // set the initial value for each field
+    productForm.fields.name.value = product.get("name");
+    productForm.fields.cost.value = product.get("cost");
+    productForm.fields.description.value = product.get("description");
+    productForm.fields.category_id.value = product.get("category_id");
+    productForm.fields.image_url.value = product.get("image_url");
+
+    // https://bookshelfjs.org/api.html#Collection-instance-pluck - returns an array of values based on the selected column name
+    const selectedTags = await product.related("tags").pluck("id");
+    productForm.fields.tags.value = selectedTags;
+
+    res.render("products/update", {
+      form: productForm.toHTML(bootstrapField),
+      product: product.toJSON(), //  convert the Product bookshelf object to the JSON format so hbs files can use it
+      cloudinaryName: process.env.CLOUDINARY_NAME,
+      cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+      cloudinaryPreset: process.env.CLOUDINARY_UPLOAD_PRESET,
+    });
+  }
+);
+
+// update the product
+router.post(
+  "/update/:product_id",
+  checkIfAuthenticated,
+  async function (req, res) {
+    const productId = req.params.product_id;
+
+    let categories = await getAllCategories();
+    let tags = await getAllTags();
+    const productForm = createProductForm(categories, tags);
+
+    productForm.handle(req, {
+      success: async function (form) {
+        await updateProductByID(productId, form.data);
+        res.redirect("/products");
+      },
+      error: function (form) {
+        res.render("/products/update", {
+          form: form,
+          prodct: product.toJSON(),
+        });
+      },
+      empty: function (form) {
+        res.render("/products/update", {
+          form: form,
+          prodct: product.toJSON(),
+        });
+      },
+    });
+  }
+);
 
 // delete the product
-router.get("/delete/:product_id", checkIfAuthenticated, async function(req,res){
-  const productId = req.params.product_id;
-  // SELECT * where products where id = '1'
-  const product = await Products.where({
-    "id": productId
-  }).fetch({
-    "require": true
-  });
+router.get(
+  "/delete/:product_id",
+  checkIfAuthenticated,
+  async function (req, res) {
+    const product = await getProductByID(req.params.product_id);
 
-  res.render("products/delete",{
-    "product": product.toJSON()
-  })
-})
+    res.render("products/delete", {
+      product: product.toJSON(),
+    });
+  }
+);
 
-router.post("/delete/:product_id", checkIfAuthenticated, async function(req,res){
-  const productId = req.params.product_id;
-  // SELECT * where products where id = '1'
-  const product = await Products.where({
-    "id": productId
-  }).fetch({
-    "require": true
-  });
-
-  await product.destroy();
-  res.redirect("/products");
-})
+router.post(
+  "/delete/:product_id",
+  checkIfAuthenticated,
+  async function (req, res) {
+    await deleteProductByID(req.params.product_id);
+    res.redirect("/products");
+  }
+);
 
 module.exports = router;
