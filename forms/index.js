e7d8b4b614243a785bed96a2c8d896826d576b80
Jin G <gao-jin@outlook.com>
2023-05-21 16:10:51 +0000

    lab 8 - step 8 creating forms

---
+++ forms/index.js
@@ -0,0 +1,56 @@
+// import forms module
+const forms = require("forms");
+// create some shortcuts
+const fields = forms.fields;
+const validators = forms.validators;
+
+
+// implements the bootstrapField helper function
+var bootstrapField = function (name, object) {
+    if (!Array.isArray(object.widget.classes)) { object.widget.classes = []; }
+
+    if (object.widget.classes.indexOf('form-control') === -1) {
+        object.widget.classes.push('form-control');
+    }
+
+    var validationclass = object.value && !object.error ? 'is-valid' : '';
+    validationclass = object.error ? 'is-invalid' : validationclass;
+    if (validationclass) {
+        object.widget.classes.push(validationclass);
+    }
+
+    var label = object.labelHTML(name);
+    var error = object.error ? '<div class="invalid-feedback">' + object.error + '</div>' : '';
+
+    var widget = object.widget.toHTML(name, object);
+    return '<div class="form-group">' + label + widget + error + '</div>';
+};
+
+
+const createRoomForm = () => {
+    return forms.create({
+        'name': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'cost': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'description': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+    })
+};
+
+module.exports = { createRoomForm, bootstrapField };
--- routes/rooms.js
+++ routes/rooms.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const router = express.Router(); // #1 - Create a new express Router
 
+// Lab 8 : import in the Forms
+const { bootstrapField, createRoomForm } = require('../forms');
+
 // #1 import in the Product model
 const {Room} = require('../models')
 
@@ -18,6 +21,35 @@ router.get('/', async (req,res)=>{
     let rooms = await Room.collection().fetch();
     res.render('rooms/index', {
         'rooms': rooms.toJSON() // #3 - convert collection to JSON
+    })
+})
+
+// lab8 : add create function to render the form 
+router.get('/create', async (req, res) => {
+    const productForm = createProductForm();
+    res.render('products/create',{
+        'form': productForm.toHTML(bootstrapField)
+    })
+})
+
+// lab8 : Process the submitted form
+router.post('/create', async(req,res)=>{
+    const productForm = createProductForm();  //first create a productForm object
+    productForm.handle(req, {  //use its handle function to process the request. 
+        //an object which contains a success function, which is run when the form is successfully processed. 
+        'success': async (form) => {   // The first argument to the success function is the form itself
+            
+            const product = new Product();
+            // retrieve the form data using form.data
+            // then use the form data to create a new instance of the Product model, and then save it.
+
+            product.set('name', form.data.name);
+            product.set('cost', form.data.cost);
+            product.set('description', form.data.description);
+            await product.save();
+            res.redirect('/products');
+            // An instance of the model represents one row in the table. 
+        }
     })
 })
 
